3a12c3bfe94adb10840180a20936ceb81ac38287
Fatima Zia <fatimazia210@gmail.com>
2026-01-21 10:36:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+running=True
+correct_ans=random.randint(1,51)
+att=0
+while True:
+    att+=1
+    user_guess=int(input())
+    if user_guess>correct_ans:
+        print("That's above the target!")
+    elif user_guess<correct_ans:
+        print("That's below the target!")
+    else:
+        print(f"Correct! You guessed it in {att} attempts!")
+        running=False
